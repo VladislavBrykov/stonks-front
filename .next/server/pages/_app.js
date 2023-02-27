2fc62e8cbf250934d6e60a20c25f79a10f227992
Vladislav Brykov <vlad5brykov@gmail.com>
2023-02-27 18:09:12 +0200

    test

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,4 +1,3 @@
-"use strict";
 (() => {
 var exports = {};
 exports.id = 888;
@@ -8,12 +7,15 @@ exports.modules = {
 /***/ 6505:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (/* binding */ App)
 /* harmony export */ });
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(108);
+/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_css__WEBPACK_IMPORTED_MODULE_1__);
 
 
 function App({ Component , pageProps  }) {
@@ -25,9 +27,17 @@ function App({ Component , pageProps  }) {
 
 /***/ }),
 
+/***/ 108:
+/***/ (() => {
+
+
+
+/***/ }),
+
 /***/ 997:
 /***/ ((module) => {
 
+"use strict";
 module.exports = require("react/jsx-runtime");
 
 /***/ })
