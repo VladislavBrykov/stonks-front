37a60b7d748f1def9e954e8664d6eec033da1a91
Vladislav Brykov <vlad5brykov@gmail.com>
2023-03-10 12:47:15 +0200

    Some fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import {Header} from "../components/header";
-import {Main} from "@/components/main";
-import {Footer} from "@/components/footer";
+import {Telegram} from "@/components/telegram";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +17,9 @@ export default function Home() {
       </Head>
       <main>
           {/*<p>fgag</p>*/}
-        <Header/>
-          <Main/>
-          <Footer/>
+        {/*<Header/>*/}
+          <Telegram/>
+          {/*<Footer/>*/}
       </main>
     </>
   )
